eed933936c49b5b0deb81960b0ff00f94600be55
lolo121 <fooksing121@gmail.com>
2015-01-29 10:37:28 +0800

    test

---
+++ sqliteSample.py
@@ -0,0 +1,21 @@
+#sqlite build sample
+import sqlite3, os
+
+conn = sqlite3.connect('example.db')
+
+c = conn.cursor()
+
+c.execute('DROP TABLE IF EXISTS foods')
+
+c.execute('''CREATE TABLE foods (name text, qty real, cost int)''')
+
+c.execute('''INSERT INTO foods VALUES ('sandwich', 30.14, 20)''')
+
+c.execute('SELECT * FROM foods')
+
+print c.fetchone()
+
+conn.commit()
+conn.close()
+
+os.system('rm example.db')
